7c0dd44432724589ce1da4bfbb267615fd3f3557
Zhanghao <yukiniro@hotmail.com>
2023-04-20 20:15:01 +0800

    feat: add some antd componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
-import { ReactComponent as LogoSvg } from "./icon/logo.svg";
-import { ReactComponent as FaviconSvg } from "./icon/favicon.svg";
+import { useCallback } from "react";
+import { Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import {} from "file-to-any";
+
+const { Dragger } = Upload;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleChange = useCallback(e => {
+    const { file } = e;
+    const blob = new Blob([file]);
+    const stream = blob.stream();
+    const reader = stream.getReader();
+  }, []);
+
+  const handleCustomRequest = useCallback(e => {
+    const { onSuccess } = e;
+    onSuccess();
+  }, []);
+
   return (
-    <div className="w-screen h-screen bg-gray-800 text-center flex flex-col items-center justify-center">
-      <h1 className="text-white text-20 font-mono font-600">Hello World</h1>
-      <div className="flex p-14 w-110 justify-between items-center">
-        <LogoSvg className="w-60 h-60" />
-        <FaviconSvg className="w-40 h-40" />
-      </div>
-      <p>
-        <button
-          className="cursor-pointer text-6 px-6 py-2 rounded-4 border-none"
-          onClick={() => setCount(count => count + 1)}
-        >
-          count is: {count}
-        </button>
-      </p>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Dragger
+        fileList={[]}
+        customRequest={handleCustomRequest}
+        onChange={handleChange}
+        style={{ padding: 20 }}
+      >
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+      </Dragger>
     </div>
   );
 }
