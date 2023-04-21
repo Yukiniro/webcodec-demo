ebd2d4b021d796d06d066c0b72e25bca5df7160f
Zhanghao <yukiniro@hotmail.com>
2023-04-21 18:42:48 +0800

    update something

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
 import { useCallback } from "react";
-import { Upload } from "antd";
+import { Upload, message } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
 import {} from "file-to-any";
 
 const { Dragger } = Upload;
 
 function App() {
+  const [messageApi, contextHolder] = message.useMessage();
   const handleChange = useCallback(e => {
     const { file } = e;
-    const blob = new Blob([file]);
-    const stream = blob.stream();
-    const reader = stream.getReader();
+    const originFileObj = file.originFileObj;
+
+    (async () => {
+      const stream = originFileObj.stream();
+      const mediaStream = new MediaStream(stream);
+      console.log(mediaStream);
+    })();
+
+    // (async () => {
+    //   try {
+    //     const stream = originFileObj.stream();
+    //     const reader = stream.getReader();
+    //     const config = {
+    //       codec: "avc1.42E01E",
+    //       codedHeight: 1080,
+    //       codedWidth: 1920,
+    //       optimizeForLatency: true,
+    //     };
+    //     const decoder: VideoDecoder = new VideoDecoder({
+    //       output: frame => {},
+    //       error: err => {},
+    //     });
+    //     await VideoDecoder.isConfigSupported(config);
+    //     decoder.configure(config);
+
+    //     while (true) {
+    //       const { done, value: chunk } = await reader.read();
+    //       if (done) {
+    //         messageApi.info("done!");
+    //         break;
+    //       }
+    //       decoder.decode(new EncodedVideoChunk())
+    //       console.log(chunk);
+    //     }
+    //   } catch (e) {
+    //     message.error(e.message);
+    //   }
+    // })();
   }, []);
 
   const handleCustomRequest = useCallback(e => {
@@ -20,6 +56,7 @@ function App() {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
+      {contextHolder}
       <Dragger
         fileList={[]}
         customRequest={handleCustomRequest}
